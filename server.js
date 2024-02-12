6fcc0b528230e5233bf6756c5f8e262c8f17be90
Simon Moore <simonmoore23@gmail.com>
2024-02-12 18:29:06 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
