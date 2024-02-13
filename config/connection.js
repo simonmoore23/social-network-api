6e601bce48f2eb9a4745e40136610ca4c43b0d48
Simon Moore <simonmoore23@gmail.com>
2024-02-13 22:20:57 +0000

    folder structure & packages

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
 
-connect(connectionString);
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports = connection;
