258c3c2870bd1648ed9ee98fa4d7c7856d4de3a5
Simon Moore <simonmoore23@gmail.com>
2024-02-15 17:12:29 +0000

    port listening

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const db = require("./config/connection");
-const router = require("./routes");
+const routes = require("./routes");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router);
+app.use(express.json());
+app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
